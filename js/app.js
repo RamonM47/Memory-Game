7117767cd0e208b672a7af32b9ac32b4d1491e29
RamonM47 <aviramon.mesa@gmail.com>
2021-10-19 11:57:51 -0400

    major changes!!

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 /*-------------------------------- Constants --------------------------------*/
-let card = document.getElementsByClassName('card');
+// let card = document.getElementsByClassName('card');
 
-let cards = [card]
+// let cards = [card]
 
-let clickedCard = null;
+// let clickedCard = null;
 
 /*---------------------------- Variables (state) ----------------------------*/
 
@@ -11,39 +11,44 @@ let clickedCard = null;
 
 /*------------------------ Cached Element References ------------------------*/
 
+const cardValues = document.querySelectorAll('.card')
 
 
 /*----------------------------- Event Listeners -----------------------------*/
 
-
+cardValues.forEach(card => card.addEventListener('click', handleClick))
 
 /*-------------------------------- Functions --------------------------------*/
 
-function onCardClick(e) {
-    const target = e.currentTarget;
-
-    target.className = target.className
-        .replace('face-down', '')
-    target.className += 'done';
-
-    if (!clickedCard) {
-        clickedCard = target;
-    } else if (clickedCard) {
-
-        if (
-            clickedCard.getAttribute('class') ===
-            target.getAttribute('class')) {
-
-        } else {
-            setTimeout(() => {
-                clickedCard.className =
-                    clickedCard.className.replace('done', '') +
-                    'face-down';
-                target.className =
-                    target.className.replace('done', '') +
-                    'face-down';
-            }, 500);
-            console.log('no');
-        }
-    }
+function handleClick() {
+    console.log(cardValues)
 }
+
+// function onCardClick(e) {
+//     const target = e.currentTarget;
+
+//     target.className = target.className
+//         .replace('face-down', '')
+//     target.className += 'done';
+
+//     if (!clickedCard) {
+//         clickedCard = target;
+//     } else if (clickedCard) {
+
+//         if (
+//             clickedCard.getAttribute('class') ===
+//             target.getAttribute('class')) {
+
+//         } else {
+//             setTimeout(() => {
+//                 clickedCard.className =
+//                     clickedCard.className.replace('done', '') +
+//                     'face-down';
+//                 target.className =
+//                     target.className.replace('done', '') +
+//                     'face-down';
+//             }, 500);
+//             console.log('no');
+//         }
+//     }
+// }
