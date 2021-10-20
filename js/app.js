fc5cd2d9eebe8d7b0ee4279e4e240fe0650c2c14
RamonM47 <aviramon.mesa@gmail.com>
2021-10-20 08:18:56 -0400

    Working like it should

--- js/app.js
+++ js/app.js
@@ -1,40 +1,93 @@
+let clickedCard = null; 
+let preventClick = false;
+
+function onCardClick(e) {
+    const target = e.currentTarget;
+
+    if (
+    preventClick ||
+    target === clickedCard ||
+    target.className.includes('done')
+    ) {
+    return;
+    }
+
+    target.className = target.className.replace('face-down', '');
+    target.className += 'done';
+
+
+
+if (!clickedCard) {
+    clickedCard = target;
+} else if (clickedCard) {
+
+    if (
+        clickedCard.getAttribute('class') !==
+        target.getAttribute('class')
+    ) {
+        preventClick = true;
+        setTimeout(() => {
+            clickedCard.className =
+                clickedCard.className.replace('done', '') +
+                'face-down';
+            target.className =
+                target.className.replace('done', '') +
+                'face-down';
+            clickedCard = null;
+            preventClick = false;
+        }, 1000);
+        console.log('no');
+    } else {
+        clickedCard = null;
+    }
+
+}
 /*-------------------------------- Constants --------------------------------*/
-const squares = document.querySelectorAll('.row > .card')
-console.log(squares)
+// const squares = document.querySelectorAll('.row > .card')
+// console.log(squares)
 
 // let clickedCard = null;
 
 /*---------------------------- Variables (state) ----------------------------*/
 
-
+// let table, winner
 
 /*------------------------ Cached Element References ------------------------*/
 
-const cardValues = document.querySelectorAll('.card')
+// const cardValues = document.querySelectorAll('section > div')
 
-const message = document.querySelectorAll('#instruction')
+// const message = document.querySelector('#instruction')
+
+// const play = document.querySelector('#play')
+
+// const reShuffle = document.querySelector('#reShuffle')
 
 /*----------------------------- Event Listeners -----------------------------*/
 
-cardValues.forEach(card => card.addEventListener('click', handleClick))
+// cardValues.forEach((card) => {
+//     card.addEventListener('click', handleClick)
+//     console.log(card)
+// })
+
+// reShuffle.addEventListener('click', init)
 
 /*-------------------------------- Functions --------------------------------*/
 
-function handleClick() {
-    console.log(cardValues)
-}
+// function handleClick() {
+//     console.log(cardValues)
+// }
 
 //--------------------------------------------
 
-function onCardClick(e) {
-    const target = e.currentTarget;
+// function onCardClick(e) {
+//     const target = e.currentTarget;
 
 
 //--------------------------------------------
 
-    target.className = target.className
-        .replace('face-down', '')
-    target.className += 'done';
+    // target.className = target.className
+    //     .replace('face-down', '')
+    // target.className += 'done';
 
 //     if (!clickedCard) {
 //         clickedCard = target;
@@ -64,4 +117,27 @@ function onCardClick(e) {
 //             console.log('no');
 //         }
 //     }
+
+// init ()
+
+// function init() {
+//     table = [null, null, null, null, null]
+
+//     render()
+// }
+
+// function render() {
 }
+
+// function shuffle(cardValues) {
+//     cardValues.sort(() => Math.random() - 0.5);
+//     console.log(shuffle)
+// }
+
+// function handleClick(evt) {
+//     let cardIdx = parseInt(evt.target.id.replace('face-down', ''))
+
+//     if (table[cardIdx] || winner) {
+//         return
+//     }
+// table[sqIdx] = tufunction onCardClick(e) {
